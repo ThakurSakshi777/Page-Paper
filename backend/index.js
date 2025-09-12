694a68b9cb86add569d3fe124dc78e84338a88dd
sakshi singh <thakursakshisingh8318@gmail.com>
2025-09-12 20:22:31 +0530

    Init the connection to database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import bookRoute from "./route/book.route.js";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+dotenv.config();
+
+const PORT = process.env.Port || 4000 ;
+const URI = process.env.MongoDBURI;
+
+// connect to mongoDB
+try {
+  mongoose.connect(URI ,{
+   
+  }) ;
+  console.log("Connection to mongoDB")
+  
+} catch (error) {
+  console.log("Error :" , error)
+}
+
+// defining routes
+app.use("/book" , bookRoute)
+
+app.listen(PORT, () => {
+  console.log(`server is  listening on port ${PORT}`)
 })
